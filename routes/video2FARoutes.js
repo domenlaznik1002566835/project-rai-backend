d399892e95233be87e227b44f620804c891eb3cf
nikda <nik.damis@gmail.com>
2024-06-08 19:02:50 +0200

    video upload works

--- routes/video2FARoutes.js
+++ routes/video2FARoutes.js
@@ -1,3 +1,4 @@
+// routes/video2FARoutes.js
 const express = require('express');
 const router = express.Router();
 const video2FAController = require('../controllers/video2FAController');
@@ -7,5 +8,7 @@ router.post('/upload', video2FAController.uploadAndVerifyVideo);
 router.get('/:id', video2FAController.getVideo);
 router.post('/verify', video2FAController.verifyVideo);
 router.post('/authenticate', video2FAController.authenticate);
+router.post('/upload-video', video2FAController.uploadVideo);
+
 
 module.exports = router;
