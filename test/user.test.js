0e9c358fa53863f6cf11fdee45076c5eb6912ac9
domen <domen.laznik@student.um.si>
2024-06-09 16:54:10 +0200

    SKUPINA7-40: user test update

--- test/user.test.js
+++ test/user.test.js
@@ -1,10 +1,10 @@
-const chai = require('chai');
+import chai from 'chai';
 const expect = chai.expect;
-const ClientController = require('../controllers/clientController'); // replace with your actual UserController path
+import ClientController from '../controllers/clientController';
 
-describe('UserController', function() {
-    it('getUser should return a user with the correct id', async function() {
-        const user = await ClientController.getByEmail("email@gmail.com")
+describe('ClientController', function() {
+    it('getByEmail should return a user with the correct firstName', async function() {
+        const user = await ClientController.getByEmail("email@gmail.com");
 
         expect(user).to.have.property('firstName', "firstname");
     });
